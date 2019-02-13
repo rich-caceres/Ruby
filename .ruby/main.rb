43e88c7b893397f701a9d30cb7b96b6316e78c51
rich-caceres <richard.caceres22590@outlook.com>
2019-02-13 11:02:17 -0500

    still getting familiar with ruby

--- .ruby/main.rb
+++ .ruby/main.rb
@@ -1,6 +1,8 @@
-puts "hello world"
+
 puts 2+2
 message= "hello, world"
 name= "Richard"
 puts message.upcase + " " + name.upcase
-puts name.length
+puts name.length 
+puts name.capitalize
+puts message.capitalize
