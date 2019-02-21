6beea3b5b31ebe826e556aaa3d584d612018c59d
rich-caceres <richard.caceres22590@outlook.com>
2019-02-21 13:59:20 -0500

    Always three silly program

---
+++ .ruby/Ruby_challenge/always_three.rb
@@ -0,0 +1,15 @@
+puts "give me a number"
+
+first_number= gets.to_i
+
+second_number= first_number + 5
+
+third_number= second_number * 2
+
+fourth_number= third_number - 4
+
+fifth_number= fourth_number / 2
+
+final_number= fifth_number - first_number
+
+puts "The number is always = #{final_number}"
